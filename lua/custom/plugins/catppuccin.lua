638e8cead0e79d02e636ee300cbebae41b7e925b
Justin Bird <justin@thewordisbird.dev>
2024-02-02 15:27:36 -0800

    add catppuccin theme

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    local catppuccin = require("catppuccin")
+
+    catppuccin.setup({})
+    vim.cmd.colorscheme "catppuccin-mocha"
+  end
+}
