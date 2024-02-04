bc1d41b3c94a9cdbce0f4f221a0f2bb435672139
Justin Bird <justin@thewordisbird.dev>
2024-02-04 10:01:51 -0800

    move no setup required plugins to their own file

--- init.lua
+++ init.lua
@@ -29,13 +29,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
 
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
