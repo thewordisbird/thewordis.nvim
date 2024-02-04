ac5fc8b55fc58e59510045bd50839c75bf5a8849
Justin Bird <justin@thewordisbird.dev>
2024-02-04 10:19:21 -0800

    remap telescope buffers fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -48,7 +48,7 @@ return {
 
       -- Find the Git root directory from the current file's path
       local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")
-      [1]
+          [1]
       if vim.v.shell_error ~= 0 then
         print("Not a git repository. Searching on current working directory")
         return cwd
@@ -70,7 +70,7 @@ return {
 
     -- See `:help telescope.builtin`
     vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[F]ind existing [B]uffers' })
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
       require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
