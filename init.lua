c65b05eaf078c9de54f863fd64a849b800fd5f44
Justin Bird <justin@thewordisbird.dev>
2024-02-05 12:00:13 -0800

    Changed to copilot.lua with copilot-cmp and copilot-lualine (#15)

--- init.lua
+++ init.lua
@@ -52,7 +52,6 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
-
 -- document existing key chains
 require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -20,6 +20,12 @@ return {
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    end
+
     cmp.setup {
       snippet = {
         expand = function(args)
@@ -36,6 +42,13 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = getfenv
         },
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
         -- ['<Tab>'] = cmp.mapping(function(fallback)
         --   if cmp.visible() then
         --     cmp.select_next_item()
@@ -56,6 +69,7 @@ return {
         end, { 'i', 's' }),
       },
       sources = {
+        { name = 'copilot' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
       },
